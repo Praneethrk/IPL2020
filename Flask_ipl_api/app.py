d3799bc5a148ef5e1d3be96195e7127afe4b8252
Praneeth RK <praneethrk17@gmail.com>
2020-02-23 00:43:46 +0530

    Flask Api added

---
+++ Flask_ipl_api/app.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, jsonify 
+import ipldb as ipldb
+from mongoflask import MongoJSONEncoder
+
+app = Flask(__name__)
+app.json_encoder = MongoJSONEncoder
+
+
+@app.route('/')
+def welcome():
+    return jsonify({"message":"Hello World"})
+
+
+@app.route('/ipl/team/names')
+def team_names():
+    names = ipldb.get_team_names()
+    return jsonify({"Team Names":names})
+
+
+@app.route('/ipl/team/labels')
+def team_labels():
+    labels = ipldb.get_team_labels()
+    return jsonify({"Team Label":labels})
+
+
+if __name__ == "__main__":
+    app.run()
